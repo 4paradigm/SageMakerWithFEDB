f01a04ad8fe27f1203e1af13fc872b36ce6f8d7b
imotai <wangtaize@4paradigm.com>
2020-07-23 15:54:00 +0800

    fix: update fe.sh

--- fe.sh
+++ fe.sh
@@ -1,9 +1,9 @@
 spark-submit \
      --master yarn --deploy-mode cluster \
-     --num-executors 1 \
-     --executor-cores 2 \
-     --driver-memory 2g \
-     --executor-memory 2g \
+     --num-executors 2 \
+     --executor-cores 1 \
+     --driver-memory 4g \
+     --executor-memory 4g \
      --py-files ./fespark.zip \
      --jars ./fesql-spark-0.0.1-SNAPSHOT-with-dependencies.jar \
      ./fe.py
